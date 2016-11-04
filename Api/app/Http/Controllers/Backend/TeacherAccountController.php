b7cddaa62ab8ad5304506601eac0c92141eb9380
Status CTO <ayimdomnic@statuscreative.com>
2016-11-04 06:44:25 -0700

    Generate a unique teacher ID and Use it on sign up. Oh! we can have a teacher's avatar's as well :)

--- Api/app/Http/Controllers/Backend/TeacherAccountController.php
+++ Api/app/Http/Controllers/Backend/TeacherAccountController.php
@@ -11,4 +11,60 @@ use App\Http\Controllers\Controller;
 class TeacherAccountController extends Controller
 {
     //
+
+    public function __construct(Teacher $teacher)
+    {
+    	$this->middleware('auth');
+    	$this->query = $teacher;
+    }
+
+    public function index()
+    {
+    	return view('admin.teachers.index');
+    }
+
+    public function create()
+    {
+    	return view('admin.teachers.create');
+    }
+
+    public function store()
+    {
+    	$validator = Validate::make($request->all(), Teacher::$validationRules);
+
+    	if($validator->fails()){
+
+    		return redirect()->route('admin.teachers.create');
+    	}
+    	$generator = App::make('GenerateRegNumber');
+    	$reg_no = $generator->generateTeacherReg();
+
+    	// logic for adding the profile picture
+    	if ($request->hasFile('image')) {
+            $image = UploadImage::uploadImage($request->file('image'), 'teacher_photo');
+        } else {
+            $image = 'default.jpg';
+        }
+
+    	$teacher = Teacher::create(array_merge($request->except('image'),[
+    		'teacher_reg'=>$reg_no, 
+    		'profile_pix' => $image]
+    		));
+
+        User::create([
+            'username'=> $teacher->teacher_reg,
+            'userable_id' => $teacher->id,
+            'userable_type' => 'App\\Data\\Models\\Teacher'
+        ]);
+
+        return redirect()->route('admin.users.index');
+
+    }
+
+    public function show($id)
+    {
+    	$teacher = Teacher::findOrFail($id);
+
+    	return view('admin.teachers.show')->('teacher', $teacher);
+    }
 }
